e6cbc6086dcfaeb40a3065f21e37d2ded13f6229
vivatiara <80299731+vivatiara@users.noreply.github.com>
2022-11-17 11:47:53 +0700

    user & aunth

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,13 +1,20 @@
+const bcrypt = require('bcrypt');
 const User = require("../models/user")
-
-
-
-
+const jwt = require("jsonwebtoken");
+const KEY = "yessir";
 
 module.exports = {
   getAllUser: async (req, res) => {
-   
-    
+    try {
+      const users = await User.find({}, "-__v -password")
+
+      res.json({
+        message: "success get data user",
+        data: users
+      })
+    } catch (error) {
+      console.log(error);
+    }
   },
 
   getUserByID: (req, res) => {
@@ -15,7 +22,20 @@ module.exports = {
   },
 
   addUser: (req, res) => {
-   
+    const data = req.body
+
+    const saltRounds = 10
+    const hash = bcrypt.hashSync(data.password, saltRounds);
+    data.password = hash
+
+    const user = new User(data)
+
+    // console.log(user)
+    user.save()
+
+    res.json({
+      message: "data has been created!!",
+    })
   },
 
   deleteUserByID: (req, res) => {
@@ -25,6 +45,6 @@ module.exports = {
   updateUserByID: (req, res) => {
 
   },
-  
+
   
 }
--- models/user.js
+++ models/user.js
@@ -2,8 +2,14 @@ const mongoose = require('mongoose')
 const{ Schema } = mongoose
 
 const userSchema = new Schema({
+    name: String,
+    email: {
+      type: String,
+      required: true
+    },
+    password: String
     
-})
+  })
 
 const User = mongoose.model('User',userSchema)
 
